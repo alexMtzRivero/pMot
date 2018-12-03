7a83edf5d361540e8b088a6c46db86733fa0148c
Alejandro Martinez <alejandro.martinez.0598@gmail.com>
2018-12-03 08:17:29 +0100

    Initial Commit

---
+++ onVerra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onVerra
+//
+//  Created by Alejandro Martinez Rivero on 03/12/2018.
+//  Copyright © 2018 Alejandro Martinez Rivero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
