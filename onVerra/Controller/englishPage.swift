e89b25c369d168d5ed35ac6b0c77e2f5ffa3eb67
elodie <moreleloo@gmail.com>
2018-12-03 15:27:57 +0100

    scroll de merde mais ça marche

---
+++ onVerra/Controller/englishPage.swift
@@ -0,0 +1,25 @@
+//
+//  englishPage.swift
+//  onVerra
+//
+//  Created by Elodie Morel on 03/12/2018.
+//  Copyright © 2018 Alejandro Martinez Rivero. All rights reserved.
+//
+
+import UIKit
+
+class englishPage: UIView {
+
+
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
